5fb5bb15ce446c6a6695b5e58fef79da6eb546bd
Ben Moulton <ben.moulton1@icloud.com>
2026-04-10 20:52:13 +0100

    empty project

--- Ghosttown/Source/Ghosttown/Ghosttown.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Ghosttown.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ghosttown, "Ghosttown" );
-
-DEFINE_LOG_CATEGORY(LogGhosttown)
